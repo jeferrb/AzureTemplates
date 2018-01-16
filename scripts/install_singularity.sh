6587af660eb2cd89c3c337bb5b42e0d43b16f569
Your Name <you@example.com>
2018-01-16 14:36:42 -0200

    Fix Install script permissions

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -2,10 +2,11 @@
 
 VERSION=2.4
 
-apt-get install -y wget make gcc openmpi-bin openmpi-common
+sudo apt-get -y update
+sudo apt-get install -y wget make gcc openmpi-bin openmpi-common
 wget -q https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
 tar xvf singularity-$VERSION.tar.gz
 cd singularity-$VERSION
 ./configure --prefix=/usr/local
-make
+make -j
 sudo make install
