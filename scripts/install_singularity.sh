8e80ee0f28b87eabd08cd1a5720cc8e92fae32e4
Your Name <you@example.com>
2018-01-17 13:17:05 -0200

    first half only

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -11,8 +11,8 @@ cd singularity-$VERSION
 make -j
 sudo make install
 cd
-sudo mkdir /home/username/mymountpoint
-echo "${1}" > pass
-sudo bash -c 'echo "//test1diag281.file.core.windows.net/shared-fs /home/username/mymountpoint cifs nofail,vers=3.0,username=test1diag281,password=`cat pass`,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
-rm -rf pass
-sudo mount -a
+# sudo mkdir /home/username/mymountpoint
+# echo "${1}" > pass
+# sudo bash -c 'echo "//test1diag281.file.core.windows.net/shared-fs /home/username/mymountpoint cifs nofail,vers=3.0,username=test1diag281,password=`cat pass`,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+# rm -rf pass
+# sudo mount -a
