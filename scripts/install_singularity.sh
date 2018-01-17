9a996e059b3810a37041cd80e25efcd3f4c7a211
Your Name <you@example.com>
2018-01-17 14:43:14 -0200

    Restore install script

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# VERSION=2.4
+VERSION=2.4
 
-# sudo apt-get -y update
-# sudo apt-get install -y wget make gcc openmpi-bin openmpi-common
-# wget -q https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
-# tar xvf singularity-$VERSION.tar.gz
-# cd singularity-$VERSION
-# ./configure --prefix=/usr/local
-# make -j
-# sudo make install
-# cd
-# sudo mkdir /home/username/mymountpoint
-# echo "${1}" > pass
-# sudo bash -c 'echo "//test1diag281.file.core.windows.net/shared-fs /home/username/mymountpoint cifs nofail,vers=3.0,username=test1diag281,password=`cat pass`,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
-# rm -rf pass
-# sudo mount -a
+sudo apt-get -y update
+sudo apt-get install -y wget make gcc openmpi-bin openmpi-common
+wget -q https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make -j
+sudo make install
+cd
+sudo mkdir /home/username/mymountpoint
+echo "${1}" > pass
+sudo bash -c 'echo "//test1diag281.file.core.windows.net/shared-fs /home/username/mymountpoint cifs nofail,vers=3.0,username=test1diag281,password=`cat pass`,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+rm -rf pass
+sudo mount -a
