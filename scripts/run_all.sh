a3f9f8b499c78c36f04c7b440c102573b59ee40d
Your Name <you@example.com>
2018-01-22 09:03:49 -0200

    check if mountpoint exists

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,4 +5,6 @@ set -x
 for i in `seq 0 1`; do
 ./scripts/run_mpi_benchmark.sh Senha123 gGEn7CeoUxlkf/EY6sUlrZFg4ebJw3ZkjJ0QvZ5viW0ES+bRDllVwLQy17M9PcWaM4PoRGhqycd9BFE7OadAqg== ${i}
 done
-# cp -r *result* ~/mountpoint/results
+if [ -d "~/mountpoint/results" ]; then
+  cp -r *result* 
+fi
