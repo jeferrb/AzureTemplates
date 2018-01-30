1244a7f0d4f487523f0b2eecb4513a8e08b4cc1e
Your Name <you@example.com>
2018-01-30 17:16:47 -0200

    New resource image - v7

--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -13,6 +13,7 @@ run_bench() {
   local nprocs="${3}"
   local repetitions="${4}"
   local path="${5}"
+  local nprocessors="${6}"
   local name="${bench}.${class}.${nprocs}"
 
   nohup sar -o "${name}_native.sa" 5 > /dev/null 2>&1 &
@@ -20,7 +21,7 @@ run_bench() {
   for i in `seq ${repetitions}`; do
     echo "Running ${name}_native (${i}/${repetitions})" | tee -a "${name}_native.log"
     date | tee -a "${name}_native.log"
-    mpirun -np "${nprocs}" -mca plm_rsh_args "-o StrictHostKeyChecking=no"  --hostfile hostfile "${path}${name}" | tee -a "${name}_native.log"
+    mpirun -np "${nprocessors}" -mca plm_rsh_args "-o StrictHostKeyChecking=no"  --hostfile hostfile "${path}${name}" | tee -a "${name}_native.log"
     date | tee -a "${name}_native.log"
     echo | tee -a "${name}_native.log"
   done
@@ -31,7 +32,7 @@ run_bench() {
   for i in `seq ${repetitions}`; do
     echo "Running ${name}_singularity (${i}/${repetitions})" | tee -a "${name}_singularity.log"
     date | tee -a "${name}_singularity.log"
-    mpirun -np "${nprocs}" -mca plm_rsh_args "-o StrictHostKeyChecking=no"  --hostfile hostfile singularity exec /home/username/ubuntu.img "${path}${name}" | tee -a "${name}_singularity.log"
+    mpirun -np "${nprocessors}" -mca plm_rsh_args "-o StrictHostKeyChecking=no"  --hostfile hostfile singularity exec /home/username/ubuntu.img "${path}${name}" | tee -a "${name}_singularity.log"
     date | tee -a "${name}_singularity.log"
     echo | tee -a "${name}_singularity.log"
   done
@@ -40,9 +41,9 @@ run_bench() {
 }
 
 for class in A ; do # B C D; do
-  run_bench lu "${class}" ${TOTAL_CORES} ${NUMBER_REPETITIONS} ${BIN_PATH}
-  run_bench sp "${class}" ${TOTAL_CORES} ${NUMBER_REPETITIONS} ${BIN_PATH}
-  run_bench bt "${class}" ${TOTAL_CORES} ${NUMBER_REPETITIONS} ${BIN_PATH}
+  run_bench lu "${class}" 32 ${NUMBER_REPETITIONS} ${BIN_PATH} ${TOTAL_CORES}
+  run_bench sp "${class}" 36 ${NUMBER_REPETITIONS} ${BIN_PATH} ${TOTAL_CORES}
+  run_bench bt "${class}" 36 ${NUMBER_REPETITIONS} ${BIN_PATH} ${TOTAL_CORES}
 done
 
 
