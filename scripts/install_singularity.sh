c1a7bfa1a7002965c3937d3d6abb7f14f53621e2
Your Name <you@example.com>
2018-05-30 21:12:37 -0300

    Enable perf execution on install script

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -49,6 +49,15 @@ VERSION=2.4.2
 cd /home/username/mymountpoint/singularity-$VERSION
 sudo make install | tee -a ${LOGFILE}
 
+
+#enable Perf
+sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
+sudo sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
+
+sudo sh -c 'echo kernel.perf_event_paranoid=1 >> /etc/sysctl.d/local.conf'
+sudo sh -c 'echo kernel.kptr_restrict=0 >> /etc/sysctl.d/local.conf'
+
+
 wait
 
 echo "done" > /home/username/donefile.txt
