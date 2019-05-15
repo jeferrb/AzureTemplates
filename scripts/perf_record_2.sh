48524e069fa1a40e7bcd42d886cfe64838cbf077
Your Name <you@example.com>
2019-05-15 16:16:13 -0300

    New perf record and stat scripts

--- scripts/perf_record_2.sh
+++ scripts/perf_record_2.sh
@@ -4,12 +4,13 @@
 
 set -x
 
-# LOG_FILE=${PWD}/logfile_execution.txt
-# echo Starting: > $LOG_FILE
-# date >> $LOG_FILE
+LOG_FILE=logfile_execution.txt
+echo Starting: > $LOG_FILE
+date >> $LOG_FILE
 # name=`echo $2 | rev | cut -d'/' -f1 | rev`
 name=${1##*/}
 out_name="${HOME}/${PWD##*/}_${name}-${RANDOM}${RANDOM}.perf.data"
-(time perf record -o ${out_name} "$@" ${out_name}) # 2>&1 | tee -a $LOG_FILE
-# echo Ending: >> $LOG_FILE
-# date >> $LOG_FILE
+(time perf record -o ${out_name} "$@" ${out_name}) 2>&1 | tee -a $LOG_FILE
+echo Ending: >> $LOG_FILE
+date >> $LOG_FILE
+
--- scripts/perf_stat.sh
+++ scripts/perf_stat.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Usage: $ mpirun -np 4 ../perf_record_2.sh ../toy2dac/bin/toy2dac
+# Usage: $ mpirun -np 4 ../perf_stat.sh ../toy2dac/bin/toy2dac
 
 set -x
 
-# LOG_FILE=${PWD}/logfile_execution.txt
+LOG_FILE=${PWD}/logfile_execution.txt
 # echo Starting: > $LOG_FILE
 # date >> $LOG_FILE
 # name=`echo $2 | rev | cut -d'/' -f1 | rev`
 name=${1##*/}
 out_name="${HOME}/${PWD##*/}_${name}-${RANDOM}${RANDOM}.perf.data"
-(time perf record -o ${out_name} "$@" ${out_name}) # 2>&1 | tee -a $LOG_FILE
+(time perf stat -o ${out_name} "$@" ${out_name}) # 2>&1 | tee -a $LOG_FILE
 # echo Ending: >> $LOG_FILE
 # date >> $LOG_FILE
