4e7a444ac0ff859d0244fcd16319352f22b8e6a0
Your Name <you@example.com>
2019-05-17 09:47:17 -0300

    Quick fix

--- scripts/run_scalability_tests_v3.py
+++ scripts/run_scalability_tests_v3.py
@@ -33,23 +33,23 @@ import collections, subprocess, os, datetime, sys
 ]'''
 
 exp64=[
-	["Standard_F16s_v2",144,16,1],
-	["Standard_F16s_v2",144,16,16],
-	["Standard_D16_v3",45,16,4],
-	["Standard_F16s_v2",144,16,3],
-	["Standard_E16_v3",98,16,4],
-	["Standard_F16s",116,16,4],
-	["Standard_D32_v3",55,32,2],
-	["Standard_F16s_v2",144,16,4],
-	["Standard_D64_v3",64,64,1],
-	["Standard_F64s_v2",146,64,1],
-	["Standard_D64s_v3",65,64,1],
-	["Standard_F16s_v2",144,16,1],
-	["Standard_F32s_v2",145,32,2],
-	["Standard_E32_v3",102,32,2],
-	["Standard_F16s_v2",144,16,2],
+	# ["Standard_F16s_v2",144,16,1],
+	# ["Standard_F16s_v2",144,16,16],
+	# ["Standard_D16_v3",45,16,4],
+	# ["Standard_F16s_v2",144,16,3],
+	# ["Standard_E16_v3",98,16,4],
+	# ["Standard_F16s",116,16,4],
+	# ["Standard_D32_v3",55,32,2],
+	# ["Standard_F16s_v2",144,16,4],
+	# ["Standard_D64_v3",64,64,1],
+	# ["Standard_F64s_v2",146,64,1],
+	# ["Standard_D64s_v3",65,64,1],
+	# ["Standard_F16s_v2",144,16,1],
+	# ["Standard_F32s_v2",145,32,2],
+	# ["Standard_E32_v3",102,32,2],
+	["Standard_F64s_v2",146,64,4],
 	["Standard_E64_v3",108,64,1],
-	["Standard_F64s_v2",146,64,4]
+	["Standard_F16s_v2",144,16,2]
 ]
 
 # print ("Usage: %s [binary_dir]"% sys.argv[0])
@@ -84,7 +84,7 @@ for exp in exp64:
 	experiments=[[exp[2]]*exp[3]]
 	print('\n\n\n\n\nRunning: azure_machine_name',azure_machine_name,'azure_machine_num',azure_machine_num, 'experiments', experiments, '\n\n\n\n')
 	today_str = now.strftime("%d-%m-%Y")
-	group_name = "gru-%d-%s-%d-%d-%s"%(azure_machine_num, azure_machine_name.replace('_','-'), exp[2], exp[3], today_str)
+	group_name = "gr-%d-%s-%d-%d-%s"%(azure_machine_num, azure_machine_name.replace('_','-'), exp[2], exp[3], today_str)
 	script_name = os.path.realpath('./scripts/run_mpi_benchmark_v10.sh')
 	base_cmd = ' '.join(['bash', script_name, group_name])
 	for experiment in experiments:
